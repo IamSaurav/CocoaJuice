b365286be798fa6c7481bb7dd212f4431c30cae5
Saurav Satpathy <saurav.satpathy@techendeavour.com>
2016-10-21 18:52:52 +0530

    Added iOS 10 sample of user notification

---
+++ UserNotification/UserNotification/AppDelegate.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Foundation;
+using UIKit;
+using UserNotifications;
+
+namespace UserNotification
+{
+
+	[Register("AppDelegate")]
+	public class AppDelegate : UIApplicationDelegate
+	{
+		public override UIWindow Window
+		{
+			get;
+			set;
+		}
+
+		public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+		{
+			ConfigureUserNotification();
+			return true;
+		}
+
+		private void ConfigureUserNotification()
+		{
+			UNUserNotificationCenter center = UNUserNotificationCenter.Current;
+			center.Delegate = new NotificationDelegate();
+			UNAuthorizationOptions options = UNAuthorizationOptions.Sound | UNAuthorizationOptions.Alert | UNAuthorizationOptions.Badge;
+			center.RequestAuthorization(options, (bool granted, NSError error) =>
+			 {
+				 if(granted && error == null)
+				 {
+					 Console.WriteLine("Will register");
+					 UIApplication.SharedApplication.RegisterForRemoteNotifications();
+				 }
+			 });
+		}
+
+		public override void RegisteredForRemoteNotifications(UIApplication application, NSData deviceToken)
+		{
+			string token = deviceToken.Description.Trim('<').Trim('>');
+			Console.WriteLine("Token : " + token);
+		}
+	}
+
+
+	public class NotificationDelegate : UNUserNotificationCenterDelegate
+	{
+		public override void WillPresentNotification(UNUserNotificationCenter center, UNNotification notification, System.Action<UNNotificationPresentationOptions> completionHandler)
+		{
+			Console.WriteLine("WillPresentNotification" + notification.Description);
+			ShowAlert("UserNotification", notification.Description);
+		}
+
+		public override void DidReceiveNotificationResponse(UNUserNotificationCenter center, UNNotificationResponse response, System.Action completionHandler)
+		{
+			Console.WriteLine("DidReceiveNotificationResponse : " + response.Notification.Description);
+			ShowAlert("UserNotification", response.Notification.Description);
+		}
+
+		private void ShowAlert(string title, string message)
+		{
+			UIAlertController alert = UIAlertController.Create(title, message, UIAlertControllerStyle.Alert);
+			alert.AddAction(UIAlertAction.Create("Ok", UIAlertActionStyle.Cancel, null));
+			AppDelegate appDelegate = UIApplication.SharedApplication.Delegate as AppDelegate;
+			appDelegate.Window.RootViewController.PresentViewController(alert, true, null);
+		}
+	}
+
+
+}
---
+++ UserNotification/UserNotification/ViewController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using UIKit;
+
+namespace UserNotification
+{
+	public partial class ViewController : UIViewController
+	{
+		protected ViewController(IntPtr handle) : base(handle)
+		{
+
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			Title = "User Notification";
+		}
+
+		public override void DidReceiveMemoryWarning()
+		{
+			base.DidReceiveMemoryWarning();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
